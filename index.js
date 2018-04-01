67cbe5a54c6755945371e2e7bc89503e6a7cb342
Syed Aman <sna274@nyu.edu>
2018-04-01 15:49:07 -0500

    add in newline to response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World!');
+  res.end('Hello World!\n');
 });
 
 server.listen(port, () => {
