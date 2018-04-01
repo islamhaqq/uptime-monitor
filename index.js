b8c754c6613fb31324fa5b7fbfe883a098a3e7a4
Syed Aman <sna274@nyu.edu>
2018-04-01 17:02:25 -0500

    log request method

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   const {pathname} = url.parse(req.url, true)
+  const {method} = req;
+
   // remove trailing and leading slashes
   const trimmedPath = pathname.replace(/^\/+|\/+$/g, '')
   res.end('Hello World!\n');
 
-  console.log(`request received on path: ${trimmedPath}`)
+  console.log(`request received on path: ${trimmedPath} with method: ${method}`)
 });
 
 server.listen(port, () => {
