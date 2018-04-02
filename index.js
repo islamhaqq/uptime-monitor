dd17220b3ba7427b0d08388f071a2a17ef5db439
Syed Aman <sna274@nyu.edu>
2018-04-01 21:03:09 -0500

    parse request payload

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  const {pathname} = url.parse(req.url, true)
-  const {method} = req;
+  const { pathname, query } = url.parse(req.url, true)
+  const { method, headers } = req;
+
+  const decoder = new StringDecoder('utf-8');
+  let payload = '';
+  req.on('data', data => {
+    payload += decoder.write(data);
+  });
+  req.on('end', () => {
+    payload += decoder.end();
+  })
 
   // remove trailing and leading slashes
   const trimmedPath = pathname.replace(/^\/+|\/+$/g, '')
-  res.end('Hello World!\n');
-
-  console.log(`request received on path: ${trimmedPath} with method: ${method}`)
+  res.end(`request received on path: ${trimmedPath} with method: ${method} and
+    params: ${JSON.stringify(query)} and headers: ${JSON.stringify(headers)}\n`);
 });
 
 server.listen(port, () => {
