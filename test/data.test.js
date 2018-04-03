fa0a41efbdcd0ed184e26b10ebd148a53c21984a
Syed Aman <sna274@nyu.edu>
2018-04-03 08:12:51 -0500

    add in ability to update files

--- test/data.test.js
+++ test/data.test.js
@@ -1,10 +1,21 @@
-const { create, read } = require('../lib/data');
+const { create, read, update } = require('../lib/data');
 
-create('whut', 'ayy', {"ay": "yooooo! it's a permanent vacation!"}, err => {
-  if (err) console.log(err);
-  console.log('success!!')
-});
-// read('whut', 'ay', (err, fileData) => {
+// create('whut', 'ayy', {"ay": "yooooo! it's a permanent vacation!"}, err => {
 //   if (err) console.log(err);
-//   console.log('success!!', JSON.parse(fileData));
-// })
+//   console.log('success!!')
+// });
+
+read('whut', 'ay', (err, fileData) => {
+  if (err) console.log(err);
+  console.log('success!!', JSON.parse(fileData));
+
+  update('whut', 'ay', {"bob": "the builder1"}, err => {
+    if (err) console.log(err);
+    console.log('success!!');
+
+    read('whut', 'ay', (err, fileData) => {
+      if (err) console.log(err);
+      console.log('success!!', JSON.parse(fileData));
+    });
+  });
+});
